a4601edb78810e64dd8f3a048a62e93700acc877
Daniel Lysak <Daniel_kn@live.no>
2021-08-26 17:52:46 +0200

    shouldConvert1ToI (Fails)

---
+++ Test/RomanNumbersTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumbersTest {
+    @Test
+    void shouldConvert1ToI(){
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int number) {
+        return "number";
+    }
+
+}
