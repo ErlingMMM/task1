96f216fde3e240d57bbaed991304895a0cbd3e5b
ErlingMMM <emagnusmoland@gmail.com>
2021-08-26 19:04:29 +0200

    test to 5

--- Test/RomanNumbersTest.java
+++ Test/RomanNumbersTest.java
@@ -3,21 +3,44 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumbersTest {
+    StringBuilder romanNumbers = new StringBuilder();
+
     @Test
     void shouldConvert1ToI(){
         assertEquals("I", toRoman(1));
     }
 
     private String toRoman(int number) {
-        if(number == 1) return "I";
-        if(number == 2) return "II";
-        return null;
+        if (number == 4) return "IV";
+        if (number == 5) return "V";
+        //if( number > 5) return
+        return loopAll(number).toString();
 
     }
 
 
     @Test
     void ShouldConvert2ToII(){ assertEquals ("II", toRoman(2));}
+
+    @Test
+    void ShouldConvert3ToIII(){ assertEquals ("III", toRoman(3));}
+
+    @Test
+    void ShouldConvert4ToIV(){ assertEquals ("IV", toRoman(4));}
+
+    @Test
+    void ShouldConvert5ToV(){ assertEquals ("V", toRoman(5));}
+
+
+   public StringBuilder loopAll(int number){
+
+        for (int i = 0; i < number; i++) {
+
+            romanNumbers.append("I");
+        }
+        return romanNumbers;
+    }
+
 
 
 }
